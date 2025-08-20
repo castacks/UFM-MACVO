61b5e7ca22fe985820c7d3700595f068235ee548
infinity1096 <2315465052@qq.com>
2025-08-20 13:31:53 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 """Package installation setup."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="uniflowmatch",
-    version="0.0.0",
+    version="0.1.0",
     description="UniFlowMatch Project",
     author="AirLab",
     license="BSD Clause-3",
-    packages=["uniflowmatch"],  # Directly specify the package
-    package_dir={
-        "uniflowmatch": "uniflowmatch",  # Map uniflowmatch package
-    },
+    packages=find_packages(exclude=['test']),  # Directly specify the package
     include_package_data=True,
 )
